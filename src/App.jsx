3509dd1cf3eb224e0b8554dc3afb097568890226
muppet-training <tomcurphey12@gmail.com>
2018-08-22 19:11:58 +1000

    Icons Broken

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
-import {
-	BrowserRouter,
-	Route,
-	Switch,
-	Redirect
-} from 'react-router-dom';
-import styled from 'styled-components';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import TopBar from './containers/menus/TopBar';
 
 import SignUp from './containers/screens/public/SignUp';
 
-const Container = styled.div`
-	margin: 0px;
-	padding: 0px;
-`;
+import './css/style.css';
 
 class App extends Component {
 	render() {
 		return (
-			<Container>
+			<div className="app">
 				<TopBar />
 				<BrowserRouter>
 					<Switch>
 						<Route path="/" exact component={SignUp} />
 					</Switch>
 				</BrowserRouter>
-			</Container>
+			</div>
 		);
 	}
 }
