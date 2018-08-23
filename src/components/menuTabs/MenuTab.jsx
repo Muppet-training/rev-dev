63249f6cf0667135e4710951be32b665c401b896
muppet-training <tomcurphey12@gmail.com>
2018-08-23 13:54:46 +1000

    Menu Updated

--- src/components/menuTabs/MenuTab.jsx
+++ src/components/menuTabs/MenuTab.jsx
@@ -7,10 +7,17 @@ class MenuTab extends Component {
 	}
 
 	render() {
-		const tabItems = [
+		const recipeItems = [
 			{ itemName: 'Details' },
 			{ itemName: 'Ingredients' },
 			{ itemName: 'Results' }
+		];
+		const ingredientItems = [ { itemName: 'Update Ingredient' } ];
+		const staffItems = [ { itemName: 'Update Staff' } ];
+		const packagingItems = [ { itemName: 'Update Packaging' } ];
+		const accountItems = [
+			{ itemName: 'Payment Details' },
+			{ itemName: 'Update Profile' }
 		];
 
 		return (
@@ -20,22 +27,26 @@ class MenuTab extends Component {
 				</section>
 				<section>
 					<h4>Recipe</h4>
-					<TabItem tabItems={tabItems} />
+					<TabItem tabItems={recipeItems} />
 				</section>
 				<section>
 					<h4>Ingredients</h4>
+					<TabItem tabItems={ingredientItems} />
 				</section>
 				<section>
 					<h4>Staff</h4>
+					<TabItem tabItems={staffItems} />
 				</section>
 				<section>
 					<h4>Fixed Costs</h4>
 				</section>
 				<section>
 					<h4>Packaging</h4>
+					<TabItem tabItems={packagingItems} />
 				</section>
 				<section>
 					<h4>Account</h4>
+					<TabItem tabItems={accountItems} />
 				</section>
 			</div>
 		);
