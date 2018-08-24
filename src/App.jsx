c9b583f8affbf8ec5c18646a0cd042f31201a44b
Valentin Foucault <foucault.val@gmail.com>
2018-08-24 16:15:48 +1000

    fixed App.jsx and started adding redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import * as admin from 'firebase-admin';
+
 import TopBar from './containers/menus/TopBar';
 import LeftBar from './containers/menus/LeftBar';
 
@@ -13,66 +15,67 @@ import './css/style.css';
 const isUser = true;
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			toggle: false
-		};
-	}
+  constructor(props) {
+    super(props);
 
-	menuToggleClick = () => {
-		this.setState((state) => ({
-			toggle: !state.toggle
-		}));
-	};
+    this.state = {
+      toggleMenu: false
+    };
+  }
 
-	render() {
-		const { toggle } = this.state;
-		console.log(toggle);
-		return (
-			<div
-				id="app"
-				className={
-					isUser === false ? (
-						'app'
-					) : toggle === true ? (
-						'userapp menu'
-					) : (
-						'' + 'userapp'
-					)
-				}
-			>
-				{isUser === true && (
-					<TopBar menuToggleClick={this.menuToggleClick} />
-				)}
-				{isUser === true && <LeftBar />}
-				<div className="screens">
-					<BrowserRouter>
-						<Switch>
-							{isUser === true ? (
-								<Route
-									path="/"
-									exact
-									component={Dashboard}
-								/>
-							) : (
-								<Route
-									path="/"
-									exact
-									component={SignUp}
-								/>
-							)}
-							<Route
-								path="/edit-recipe"
-								exact
-								component={EditRecipe}
-							/>
-						</Switch>
-					</BrowserRouter>
-				</div>
-			</div>
-		);
-	}
+  menuToggleClick = () => {
+    this.setState((state) => ({
+      toggleMenu: !state.toggleMenu
+    }));
+  };
+
+  render() {
+    const { toggleMenu } = this.state;
+    console.log(toggleMenu);
+    return (
+      <div
+        id="app"
+        className={
+          isUser === false ? (
+            'app'
+          ) : toggleMenu === true ? (
+            'userapp menu'
+          ) : (
+                '' + 'userapp'
+              )
+        }
+      >
+        {isUser === true && <TopBar menuToggleClick={this.menuToggleClick} />}
+        {isUser === true && <LeftBar />}
+        <div className="screens">
+          <BrowserRouter>
+            {isUser === true ? (
+              <Switch>
+                <Route
+                  path="/"
+                  exact={true}
+                  component={Dashboard}
+                />
+                <Route
+                  path="/edit-recipe"
+                  exact
+                  component={EditRecipe}
+                />
+              </Switch>
+            ) : (
+                <Switch>
+                  <Route
+                    path="/"
+                    exact={true}
+                    component={SignUp}
+                  />
+                </Switch>
+              )}
+          </BrowserRouter>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
